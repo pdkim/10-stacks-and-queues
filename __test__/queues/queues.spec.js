6edc4fe137c8d21d511621361d365df23cd8fa7f
Phillip Kim <phildkim@gmail.com>
2018-07-01 17:33:49 -0700

    finally passing locally

--- __test__/queues/queues.spec.js
+++ __test__/queues/queues.spec.js
@@ -1,15 +1,51 @@
 'use strict';
 
-const queue = require('../../lib/queues/queues.js');
+const Queue = require('../../lib/queues/queues.js');
 
 describe('Queues module should', () => {
 
-  xit('add an element following FIFO guideline', () => {
-    expect(null).toBeNull();
+  it('add an element following FIFO guideline', () => {
+
+    let queue = new Queue;
+    queue.push(1);
+    queue.enqueue(2);
+
+    expect(queue.foot.value).toBe(2);
+
   });
 
   
-  xit('remove an element following the FIFO guideline', () => {
-    expect(null).toBeNull();
+  it('remove an element following the FIFO guideline', () => {
+
+    let queue = new Queue;
+    queue.push(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.dequeue();
+
+    expect(queue.head.value).toBe(2);
+  });
+
+
+  it('convert a list into a string', () => {
+
+    let queue = new Queue;
+    queue.push(1);
+    queue.enqueue(2);
+
+    let expected = '1 => 2 => X';
+
+    expect(queue.serialize()).toBe(expected);
+
+  });
+
+
+  it('convert string into a new list', () => {
+
+    let queue = Queue.deserialize('1, 2, 3');
+    console.log(queue);
+
+    expect(queue.head.value).toBe(1);
+
   });
 });
--- __test__/stacks/stacks.spec.js
+++ __test__/stacks/stacks.spec.js
@@ -1,18 +1,43 @@
 'use strict';
 
-const stack = require('../../lib/stacks/stacks.js');
+const Stack = require('../../lib/stacks/stacks.js');
 
 describe('Stacks module should', () => {
 
-  xit('add an element to the array following the LIFO method', () => {
-    expect(null).toBeNull();
+  it('add an element to an empty list', () => {
+    
+    let list = new Stack;
+    expect(list.head).toBeNull();
+    
+    list.push(1);
+    expect(list.head.value).toBe(1);
+  
   });
 
-  xit('remove an element in the array following the LIFO method', () => {
-    expect(null).toBeNull();
+
+  it('add an element to the array following the LIFO method', () => {
+    
+    let list = new Stack;
+    list.push(1);
+    list.push(2);
+    expect(list.head.value).toBe(2);
+  
   });
 
-  xit('do something else cool', () => {
-    expect(null).toBeNull();
+
+  it('remove an element in the array following the LIFO method', () => {
+    
+    let list = new Stack;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    console.log('head is ', list.head.value);
+    expect(list.foot.value).toBe(3);
+
+    list.pop();
+    console.log(list.head.value);
+    expect(list.head.value).toEqual(2);
+  
   });
-})
+
+});
