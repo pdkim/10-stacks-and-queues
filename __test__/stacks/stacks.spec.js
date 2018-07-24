61a2810ddcaf7d122e2aa4214027000a37342586
Phillip Kim <phildkim@gmail.com>
2018-07-23 18:23:29 -0700

    all code and tests working

--- __test__/stacks/stacks.spec.js
+++ __test__/stacks/stacks.spec.js
@@ -31,13 +31,34 @@ describe('Stacks module should', () => {
     list.push(1);
     list.push(2);
     list.push(3);
-    console.log('head is ', list.head.value);
     expect(list.foot.value).toBe(3);
 
     list.pop();
-    console.log(list.head.value);
     expect(list.head.value).toEqual(2);
   
+  });
+
+
+  it('convert stack into a string', () => {
+
+    let stack = new Stack;
+    stack.push(1);
+    stack.push(2);
+
+    let expected = '2 => 1 => X';
+
+    expect(stack.serialize()).toBe(expected);
+  });
+
+
+  it('convert string into a new stack list', () => {
+
+    let list = Stack.deserialize('1, 2, 3');
+    
+    expect(list.head.value).toBe(3);
+    expect(list.head.next.value).toBe(2);
+    expect(list.head.next.next.value).toBe(1);
+    expect(list.head.next.next.next).toBeNull();
   });
 
 });
